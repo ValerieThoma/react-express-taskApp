b09f34b9b323f4ae61f0936ff100a5a7da598b09
Valerie Jane <valeriejanethoma@gmail.com>
2017-12-09 22:19:44 -0500

    initial commit

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,110 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var config = require('../config/config');
+
+var connection = mysql.createConnection(config);
+connection.connect();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.post('/addTask', (req,res)=>{
+	const taskName = req.body.taskName;
+	const taskDate = req.body.taskDate;
+	var thePromise = new Promise((resolve, reject)=>{
+		const insertQuery = `INSERT INTO tasks (taskName, taskDate)
+			VALUES (?,?);`;
+		connection.query(insertQuery, [taskName, taskDate], (error)=>{
+			if(error){
+				reject(error);
+			}else{
+				resolve({msg:"success"});
+			}
+		})	
+	})
+	thePromise.then((promiseResponse)=>{
+		const selectQuery = `SELECT taskName, DATE_FORMAT(taskDate, '%M %D\, %Y') as taskDate FROM tasks;`;
+		connection.query(selectQuery, (error, results)=>{
+			if (error){
+				throw error;
+			}else{
+				res.json(results);
+			}
+		})	
+	});
+})
+
+router.post('/deleteTask', function(req, res){
+	const taskId = req.body.taskId;
+	const deleteQuery = `DELETE FROM tasks WHERE id=?`;
+	var promiseOne = new Promise((resolve, reject)=>{
+		connection.query(deleteQuery, [taskId], (error, result)=>{
+			if (error){
+				reject(error)
+			}else{
+				resolve({msg: "Success"});
+			}
+		})
+	})
+	promiseOne.then((data)=>{
+		const selectQuery = `SELECT * FROM tasks;`;
+		const updateQuery = `ALTER TABLE tasks AUTO_INCREMENT = 1;`;
+		connection.query(selectQuery, (error, results)=>{
+			if (error){
+				throw error
+			}else{
+				if (results.length === 0){
+					connection.query(updateQuery)
+				}
+				res.json(results)
+			}			
+		})
+
+	})
+})
+
+router.get('/getStudents', function(req, res, next) {
+	const selectQuery = `SELECT * FROM students;`;
+	connection.query(selectQuery,(error, results)=>{
+		if (error){
+			throw error;
+		}else{
+			res.json(results);
+		}
+	})
+});
+
+router.post('/addStudent', (req, res)=>{
+	const studentName = req.body.studentName;
+	var insertQuery = `INSERT INTO students (name)
+		VALUES
+		(?);`;
+	var promiseOne = new Promise((resolve,reject)=>{
+		connection.query(insertQuery, [studentName], (error)=>{
+			if (error){
+				reject(error);
+			}else{
+				resolve({msg:"Success"});
+			}
+		})
+	});
+	promiseOne.then((data)=>{
+		var promiseTwo = new Promise((resolve, reject)=>{
+			const query = `SELECT * FROM students;`;
+			connection.query(query, (error, results)=>{
+				if(error){
+					reject(error);
+				}else{
+					resolve(results)
+				}
+
+			})
+		});
+		promiseTwo.then((studentsList)=>{
+			res.json(studentsList)
+		})
+	});		
+	// res.json(req.body);
 });
 
 module.exports = router;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ToDo from './ToDo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ToDo />, document.getElementById('root'));
 registerServiceWorker();
